9413fd46f5c8794a1f205f3e9fdc1fddbd4ed130
GRoxo-B <gui.basso@gmail.com>
2025-08-28 21:54:19 -0300

    fix: update API_URL to use production endpoint

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_URL = 'https://finance-system-api.prxlab.app/api';
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'https://finance-system-api.prxlab.app/api';
+// const API_URL = 'http://localhost:3000/api';
 
 export const api = axios.create({
   baseURL: API_URL,

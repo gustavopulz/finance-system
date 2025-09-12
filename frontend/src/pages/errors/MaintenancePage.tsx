aff02604a2e33ddf75fdcb5e74249f2381144d3e
gustavopulz <pulzgustavo1@gmail.com>
2025-09-12 19:29:40 -0300

    fix: center align text in MaintenancePage description

--- frontend/src/pages/errors/MaintenancePage.tsx
+++ frontend/src/pages/errors/MaintenancePage.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const MaintenancePage: React.FC = () => (
     <div className="flex flex-col items-center justify-center h-screen overflow-hidden bg-gray-100 dark:bg-slate-900 text-gray-800 dark:text-gray-200">
       <h1 className="text-6xl font-bold mb-4">Em manutenção :(</h1>
-      <p className="text-xl mb-6">O Finance System está desativado para o lançamento de uma nova versão.<br />Aguarde, voltamos em breve.</p>
+      <p className="text-xl mb-6 text-center">O Finance System está desativado para o lançamento de uma nova versão.<br />Aguarde, voltamos em breve.</p>
       <style>{`
         body {
           overflow: hidden;

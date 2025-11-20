096567e507a49f17867572c3aac3ec1173b99ccf
GRoxo-B <gui.basso@gmail.com>
2025-11-20 14:27:11 -0300

    refactor: change api dir name + update prisma to new schema

--- backend/prisma/migrations/20250928215946_remove_bill_paid_bollean/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `paid` on the `Bills` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "public"."Bills" DROP COLUMN "paid";
